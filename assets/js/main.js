6d458d3ab4e54f838f8925825720e967cd69faf2
Tomas Rodriguez <mazzito2015@gmail.com>
2022-06-13 20:36:06 -0400

    Order section terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,63 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
 
-let searchForm = document.querySelector('#search-form');
-let searchIcon = document.querySelector('#search-icon');
-let closeIcon = document.querySelector('#close');
+let searchForm = document.querySelector("#search-form");
+let searchIcon = document.querySelector("#search-icon");
+let closeIcon = document.querySelector("#close");
 
-searchIcon.onclick = () => {    
-    searchForm.classList.toggle('active');
-}
+searchIcon.onclick = () => {
+  searchForm.classList.toggle("active");
+};
 
 closeIcon.onclick = () => {
-    searchForm.classList.toggle('active');
-}
+  searchForm.classList.toggle("active");
+};
 
 // Swiper JS Code
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 1500000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1500000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
 
-
-  var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    loop:true,
-    breakpoints: {
-      0: {
-          slidesPerView: 1,
-      },
-      640: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    640: {
+      slidesPerView: 2,
     },
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
